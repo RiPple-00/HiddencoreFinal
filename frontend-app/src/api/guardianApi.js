90c82067b749b1ea15d2c1589b833d0de5acc94d
nicolekim1248-star <nicolekim1248@gmail.com>
2026-05-08 01:05:43 +0900

    프로그램 신청하기, 환자보호자 연결 필요, 목록까지만 만듬

--- frontend-app/src/api/guardianApi.js
+++ frontend-app/src/api/guardianApi.js
@@ -1,15 +1,22 @@
-import axios from "axios";
-
-// 🔥 여기가 핵심 (네 PC IP로 바꿔야함)
-const API = axios.create({
-  baseURL: "http://10.100.0.168:8080",
-});
+import api from "./index";
 
 // 공지사항
-export const getNotices = () => API.get("/api/notices");
+export const getNotices = () => api.get("/api/notices");
 
 // 식단
-export const getMeals = () => API.get("/api/meals");
+export const getMeals = () => api.get("/api/meals");
 
 // 환자 정보
-export const getPatient = () => API.get("/api/patient");
+export const getPatient = () => api.get("/api/patient");
+
+// 프로그램 목록 조회
+export const getPrograms = () =>
+  api.get("/api/facilities/2/posts?type=APPLY"); //강제로 부여한 시설 ID 2 
+
+// 프로그램 신청
+export const applyProgram = (postId) =>
+  api.post(`/api/guardian/programs/${postId}/apply`);
+
+// 내 프로그램 신청내역 조회
+export const getProgramApplications = () =>
+  api.get("/api/guardian/programs/applications");
